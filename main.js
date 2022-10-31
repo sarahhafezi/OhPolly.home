e22a25a26667403adb8d3475c1db248c570555c3
sarahhafezi <sarahhafezi27@gmail.com>
2022-10-31 19:45:08 +0000

    New files added

---
+++ main.js
@@ -0,0 +1,12 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "475px";
+  document.getElementById("main").style.marginLeft = "0px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+
+}
